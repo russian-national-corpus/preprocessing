1d71e10ccdbb0d63c71c4aabad6eeb646facfb05
Anton Dyshkant <vyshkant@gmail.com>
2019-05-02 23:37:58 +0300

    Fixed legacy external dependencies usage

--- src/Legacy/ruscorpora_tagging/modules/lemmer_lru_cache.py
+++ src/Legacy/ruscorpora_tagging/modules/lemmer_lru_cache.py
@@ -2,8 +2,8 @@ from cachetools import LRUCache
 
 
 class LemmerLRUCache(LRUCache):
-    def __init__(self, maxsize, missing=None, getsizeof=None, evict=None):
-        LRUCache.__init__(self, maxsize, missing, getsizeof)
+    def __init__(self, maxsize, getsizeof=None, evict=None):
+        LRUCache.__init__(self, maxsize, getsizeof)
         self.__evict = evict
 
     def popitem(self):
